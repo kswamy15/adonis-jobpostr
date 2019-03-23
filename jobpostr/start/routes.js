74c76fb755cda05a4381f7214ac499802ca60522
Narayana Swamy <knswamy@gmail.com>
2019-03-23 16:17:53 -0500

    jobmodel built

--- jobpostr/start/routes.js
+++ jobpostr/start/routes.js
@@ -16,4 +16,19 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+Route.get('/', 'JobController.home');
+Route.on('/signup').render('auth.signup');
+Route.post('/signup', 'UserController.create').validator('CreateUser');
+Route.on('/login').render('auth.login');
+Route.get('/logout', async ({ auth, response }) => {
+    await auth.logout();
+    return response.redirect('/');
+});
+Route.post('/login', 'UserController.login').validator('LoginUser');
+Route.get('/post-a-job', 'JobController.userIndex');
+
+Route.group(() => {
+    Route.get('/delete/:id', 'JobController.delete');
+    Route.get('/edit/:id', 'JobController.edit');
+    Route.post('/update/:id', 'JobController.update').validator('CreateJob');
+}).prefix('/post-a-job');
